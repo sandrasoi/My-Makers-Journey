f292f2bde74d525ec1aac07750a8e889b96be06f
Sandra Soinska <78183127+sandrasoi@users.noreply.github.com>
2023-09-06 17:40:06 +0100

    Add files via upload

--- My-Programs/password_validator.py
+++ My-Programs/password_validator.py
@@ -1,1 +1,30 @@
+# == INSTRUCTIONS ==
+#
+# Purpose: Validate a password
+# Parameters: one, a string
+# Rules:
+#   - It must be longer than 7 characters (8 is fine)
+#   - It must contain at least one of the following special characters: `!`, `@`,
+#     `$`, `%` or `&`
+# Returns: a boolean (True if valid, False otherwise)
+# Example:
+#   Call:    is_valid("1234567")
+#   Returns: False
+#   Call:    is_valid("12345678")
+#   Returns: False
+#   Call:    is_valid("12345!78")
+#   Returns: True
 
+# == YOUR CODE ==
+
+def is_valid(password):
+    if len(password) > 7:
+        if "!" in password or "@" in password or "$" in password or "%" in password or "&" in password:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+print(is_valid("1234567!"))
